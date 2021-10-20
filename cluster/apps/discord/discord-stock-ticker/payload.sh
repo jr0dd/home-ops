e45068a0a5d54b7a7eabd10f7eb888dd5b7b4d60
jr0dd <285797+jr0dd@users.noreply.github.com>
2021-10-20 00:37:00 -0400

    add cm generator back to bot

---
+++ cluster/apps/discord/discord-stock-ticker/payload.sh
@@ -0,0 +1,121 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o errexit
+
+curl -X POST \
+"localhost:8080/ticker" \
+-H "Content-Type: application/json" \
+-d "{\"ticker\":\"BTC\", \
+    \"name\":\"bitcoin\", \
+    \"discord_bot_token\":\"$BTC\", \
+    \"crypto\":true, \
+    \"frequency\":5, \
+    \"set_nickname\":true, \
+    \"arrows\":true, \
+    \"set_color\":true}"
+
+curl -X POST \
+"localhost:8080/ticker" \
+-H "Content-Type: application/json" \
+-d "{\"ticker\":\"DOGE\", \
+    \"name\":\"dogecoin\", \
+    \"discord_bot_token\":\"$DOGE\", \
+    \"crypto\":true, \
+    \"frequency\":5, \
+    \"set_nickname\":true, \
+    \"arrows\":true, \
+    \"set_color\":true}"
+
+curl -X POST \
+"localhost:8080/ticker" \
+-H "Content-Type: application/json" \
+-d "{\"ticker\":\"ETH\", \
+    \"name\":\"ethereum\", \
+    \"discord_bot_token\":\"$ETH\", \
+    \"crypto\":true, \
+    \"frequency\":5, \
+    \"set_nickname\":true, \
+    \"arrows\":true, \
+    \"set_color\":true}"
+
+curl -X POST \
+"localhost:8080/ticker" \
+-H "Content-Type: application/json" \
+-d "{\"ticker\":\"SHIB\", \
+    \"name\":\"shiba-inu\", \
+    \"discord_bot_token\":\"$SHIB\", \
+    \"crypto\":true, \
+    \"frequency\":5, \
+    \"set_nickname\":true, \
+    \"arrows\":true, \
+    \"set_color\":true}"
+
+curl -X POST \
+"localhost:8080/tickerboard" \
+-H "Content-Type: application/json" \
+-d "{\"name\":\"INDICES\", \
+    \"discord_bot_token\":\"$INDICES\", \
+    \"items\": [\"SPY\", \"^VIX\", \"IWM\", \"^NDX\"], \
+    \"frequency\":5, \
+    \"percentage\": true, \
+    \"set_nickname\":true, \
+    \"arrows\":true, \
+    \"set_color\":true}"
+
+curl -X POST \
+"localhost:8080/tickerboard" \
+-H "Content-Type: application/json" \
+-d "{\"name\":\"FUTURES\", \
+    \"discord_bot_token\":\"$FUTURES\", \
+    \"items\": [\"ES=F\", \"RTY=F\", \"NQ=F\", \"YM=F\"], \
+    \"frequency\":5, \
+    \"percentage\": true, \
+    \"set_nickname\":true, \
+    \"arrows\":true, \
+    \"set_color\":true}"
+
+curl -X POST \
+"localhost:8080/tickerboard" \
+-H "Content-Type: application/json" \
+-d "{\"name\":\"MEMES\", \
+    \"discord_bot_token\":\"$MEMES\", \
+    \"items\": [\"PLTR\", \"MVIS\", \"TLRY\", \"BB\", \"GME\", \"AMC\", \"WISH\", \"CLNE\", \"NOK\", \"RKT\", \"BBBY\", \"KOSS\", \"CLOV\", \"SPCE\"], \
+    \"frequency\":5, \
+    \"percentage\": true, \
+    \"set_nickname\":true, \
+    \"arrows\":true, \
+    \"set_color\":true}"
+
+curl -X POST \
+"localhost:8080/ticker" \
+-H "Content-Type: application/json" \
+-d "{\"ticker\":\"AAPL\", \
+    \"name\":\"AAPL\", \
+    \"discord_bot_token\":\"$AAPL\", \
+    \"frequency\":5, \
+    \"set_nickname\":true, \
+    \"arrows\":true, \
+    \"set_color\":true}"
+
+curl -X POST \
+"localhost:8080/ticker" \
+-H "Content-Type: application/json" \
+-d "{\"ticker\":\"DPW\", \
+    \"name\":\"DPW\", \
+    \"discord_bot_token\":\"$DPW\", \
+    \"frequency\":5, \
+    \"set_nickname\":true, \
+    \"arrows\":true, \
+    \"set_color\":true}"
+
+curl -X POST \
+"localhost:8080/ticker" \
+-H "Content-Type: application/json" \
+-d "{\"ticker\":\"TSLA\", \
+    \"name\":\"TSLA\", \
+    \"discord_bot_token\":\"$TSLA\", \
+    \"frequency\":5, \
+    \"set_nickname\":true, \
+    \"arrows\":true, \
+    \"set_color\":true}"
