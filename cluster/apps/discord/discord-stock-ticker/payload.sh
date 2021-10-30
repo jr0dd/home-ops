4f7b82b957116dc5badca4105491c5d810023856
jr0dd <285797+jr0dd@users.noreply.github.com>
2021-10-30 13:13:50 -0400

    bot adjustments

--- cluster/apps/discord/discord-stock-ticker/payload.sh
+++ cluster/apps/discord/discord-stock-ticker/payload.sh
@@ -4,6 +4,42 @@ set -o nounset
 set -o errexit
 
 curl -X POST \
+"localhost:8080/tickerboard" \
+-H "Content-Type: application/json" \
+-d "{\"name\":\"INDICES\", \
+    \"discord_bot_token\":\"$INDICES\", \
+    \"items\": [\"SPY\", \"^VIX\", \"IWM\", \"^NDX\"], \
+    \"frequency\":5, \
+    \"percentage\": true, \
+    \"set_nickname\":false, \
+    \"arrows\":true, \
+    \"set_color\":true}"
+
+curl -X POST \
+"localhost:8080/tickerboard" \
+-H "Content-Type: application/json" \
+-d "{\"name\":\"FUTURES\", \
+    \"discord_bot_token\":\"$FUTURES\", \
+    \"items\": [\"ES=F\", \"RTY=F\", \"NQ=F\", \"YM=F\"], \
+    \"frequency\":5, \
+    \"percentage\": true, \
+    \"set_nickname\":false, \
+    \"arrows\":true, \
+    \"set_color\":true}"
+
+curl -X POST \
+"localhost:8080/tickerboard" \
+-H "Content-Type: application/json" \
+-d "{\"name\":\"MEMES\", \
+    \"discord_bot_token\":\"$MEMES\", \
+    \"items\": [\"PLTR\", \"MVIS\", \"TLRY\", \"BB\", \"GME\", \"AMC\", \"WISH\", \"CLNE\", \"NOK\", \"RKT\", \"BBBY\", \"KOSS\", \"CLOV\", \"SPCE\"], \
+    \"frequency\":5, \
+    \"percentage\": true, \
+    \"set_nickname\":false, \
+    \"arrows\":true, \
+    \"set_color\":true}"
+
+curl -X POST \
 "localhost:8080/ticker" \
 -H "Content-Type: application/json" \
 -d "{\"ticker\":\"BTC\", \
@@ -47,42 +83,6 @@ curl -X POST \
     \"discord_bot_token\":\"$SHIB\", \
     \"crypto\":true, \
     \"frequency\":5, \
-    \"set_nickname\":true, \
-    \"arrows\":true, \
-    \"set_color\":true}"
-
-curl -X POST \
-"localhost:8080/tickerboard" \
--H "Content-Type: application/json" \
--d "{\"name\":\"INDICES\", \
-    \"discord_bot_token\":\"$INDICES\", \
-    \"items\": [\"SPY\", \"^VIX\", \"IWM\", \"^NDX\"], \
-    \"frequency\":5, \
-    \"percentage\": true, \
-    \"set_nickname\":true, \
-    \"arrows\":true, \
-    \"set_color\":true}"
-
-curl -X POST \
-"localhost:8080/tickerboard" \
--H "Content-Type: application/json" \
--d "{\"name\":\"FUTURES\", \
-    \"discord_bot_token\":\"$FUTURES\", \
-    \"items\": [\"ES=F\", \"RTY=F\", \"NQ=F\", \"YM=F\"], \
-    \"frequency\":5, \
-    \"percentage\": true, \
-    \"set_nickname\":true, \
-    \"arrows\":true, \
-    \"set_color\":true}"
-
-curl -X POST \
-"localhost:8080/tickerboard" \
--H "Content-Type: application/json" \
--d "{\"name\":\"MEMES\", \
-    \"discord_bot_token\":\"$MEMES\", \
-    \"items\": [\"PLTR\", \"MVIS\", \"TLRY\", \"BB\", \"GME\", \"AMC\", \"WISH\", \"CLNE\", \"NOK\", \"RKT\", \"BBBY\", \"KOSS\", \"CLOV\", \"SPCE\"], \
-    \"frequency\":5, \
-    \"percentage\": true, \
     \"set_nickname\":true, \
     \"arrows\":true, \
     \"set_color\":true}"
