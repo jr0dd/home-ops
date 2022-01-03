68d31debeb02236306e84428f31fb13fab87c48b
jr0dd <285797+jr0dd@users.noreply.github.com>
2022-01-03 12:58:22 -0500

    chore: add mailgun dns to cloudflare

--- terraform/cloudflare/dns_records.tf
+++ terraform/cloudflare/dns_records.tf
@@ -3,7 +3,9 @@ data "http" "ipv4" {
   url = "http://ipv4.icanhazip.com"
 }
 
-## Base records
+#
+# Base records
+#
 
 # Record which will be updated by DDNS
 resource "cloudflare_record" "apex_ipv4" {
@@ -39,5 +41,56 @@ resource "cloudflare_record" "cname_wireguard" {
   value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
   proxied = false
   type    = "CNAME"
+  ttl     = 1
+}
+
+#
+# Mailgun
+#
+
+resource "cloudflare_record" "cname_mailgun" {
+  name    = "email.mg"
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = "mailgun.org"
+  proxied = false
+  type    = "CNAME"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "mx_mailgun_a" {
+  name    = "mg"
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = "mxa.mailgun.org"
+  proxied = false
+  type    = "MX"
+  ttl     = 1
+  priority = 10
+}
+
+resource "cloudflare_record" "mx_mailgun_b" {
+  name    = "mg"
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = "mxb.mailgun.org"
+  proxied = false
+  type    = "MX"
+  ttl     = 1
+  priority = 10
+}
+
+resource "cloudflare_record" "txt_mailgun_spf" {
+  name    = "mg"
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = "v=spf1 include:mailgun.org ~all"
+  proxied = false
+  type    = "TXT"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "txt_mailgun_cert" {
+  name    = "k1._domainkey.mg"
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = data.sops_file.cloudflare_secrets.data["mailgun_cert"]
+  proxied = false
+  type    = "TXT"
   ttl     = 1
 }
