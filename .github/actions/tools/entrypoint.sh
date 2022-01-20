06b4ed4268116a39dd81b9d37d450743a2a37450
jr0dd <285797+jr0dd@users.noreply.github.com>
2022-01-20 00:33:01 -0500

    fix flux validate

--- .github/actions/tools/entrypoint.sh
+++ .github/actions/tools/entrypoint.sh
@@ -2,26 +2,26 @@
 
 set -eu
 
-YQ_VERSION="v4.6.1"
-KUSTOMIZE_VERSION="4.1.3"
-KUBEVAL_VERSION="0.15.0"
+YQ_VERSION="4.16.2"
+KUSTOMIZE_VERSION="4.4.1"
+KUBEVAL_VERSION="0.16.1"
 
 mkdir -p $GITHUB_WORKSPACE/bin
 
-curl -sL https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/yq_linux_amd64 -o yq
+curl -sL https://github.com/mikefarah/yq/releases/download/v${YQ_VERSION}/yq_linux_amd64 -o yq
 
 cp ./yq $GITHUB_WORKSPACE/bin
 chmod +x $GITHUB_WORKSPACE/bin/yq
 
 kustomize_url=https://github.com/kubernetes-sigs/kustomize/releases/download && \
 curl -sL ${kustomize_url}/kustomize%2Fv${KUSTOMIZE_VERSION}/kustomize_v${KUSTOMIZE_VERSION}_linux_amd64.tar.gz | \
-tar xz
+tar xzf -
 
 cp ./kustomize $GITHUB_WORKSPACE/bin
 chmod +x $GITHUB_WORKSPACE/bin/kustomize
 
-curl -sL https://github.com/instrumenta/kubeval/releases/download/${KUBEVAL_VERSION}/kubeval-linux-amd64.tar.gz | \
-tar xz
+curl -sL https://github.com/instrumenta/kubeval/releases/download/v${KUBEVAL_VERSION}/kubeval-linux-amd64.tar.gz | \
+tar xzf -
 
 cp ./kubeval $GITHUB_WORKSPACE/bin
 chmod +x $GITHUB_WORKSPACE/bin/kubeval
